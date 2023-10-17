483e60315e2cb6507ac14b0bd662416b830d6968
davitacols <98448386+davitacols@users.noreply.github.com>
2023-10-17 17:07:52 +0100

    added search box

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -9,27 +9,35 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarsFurni">
-            <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarsFurni">
+            <ul class="custom-navbar-nav navbar-nav mb-2 mb-md-0">
                 <li class="nav-item active">
                     <a class="nav-link" href="{{ url('index.html') }}">Home</a>
                 </li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('shop.html') }}">Shop</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('about.html') }}">About us</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('services.html') }}">Services</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ url('blog.html') }}">Blog</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ url('contact.html') }}">Contact us</a></li>
+
+                <!-- Search Box -->
+                <li class="nav-item">
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Find Item">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </li>
+                
+                
 
                 <!-- Authentication Links -->
                 @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <div class="d-flex">
                         @auth
                             <div class="dropdown">
-                            <button class="btn btn-secondary btn-xs dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                                {{ Auth::user()->name }}
-                            </button>
+                                <button class="btn btn-secondary btn-xs dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ Auth::user()->name }}
+                                </button>
                                 <ul class="dropdown-menu" aria-labelledby="userDropdown">
-                                    
+                                    <li><a class="dropdown-item" href="{{ url('/home') }}">Settings</a></li>                              
                                     <li>
                                         <form method="POST" action="{{ route('logout') }}">
                                             @csrf
@@ -39,13 +47,12 @@
                                 </ul>
                             </div>
                         @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                            <a href="{{ route('login') }}" class="btn btn-primary small-btn mx-2">Log in</a>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                <a href="{{ route('register') }}" class="btn btn-secondary small-btn mx-2">Register</a>
                             @endif
                         @endauth
-                        
                     </div>
                 @endif
             </ul>
--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -2,14 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
-                <div class="hero-content">
-                    <h1>Welcome to <span class="highlight-text">Fine Wood Furniture</span></h1>
-                    <p class="hero-description">Elevate your living space with our exquisite handcrafted furniture. Redefine quality and style.</p>
-                    <div class="hero-buttons">
-                        <a href="#" class="btn btn-primary">Shop Now</a>
-                        <a href="#" class="btn btn-secondary">Explore</a>
-                    </div>
+            <div class="hero-content text-center">
+                <h1>Welcome to <span class="highlight-text">Fine Wood Furniture</span></h1>
+                <p class="hero-description">Elevate your living space with our exquisite handcrafted furniture, blending timeless elegance with modern comfort. Rediscover the joy of home with our meticulously crafted pieces that seamlessly merge sophistication and practicality. Redefine your lifestyle with our curated designs, where every detail speaks of quality and refinement. Experience the art of living – where comfort meets luxury and style meets substance.</p>
+                <div class="hero-buttons mt-4">
+                    <a href="#" class="btn btn-primary mx-2">Shop Now</a>
+                    <a href="#" class="btn btn-secondary mx-2">Explore</a>
                 </div>
+            </div>
+
+            <div id="countdown" class="countdown-timer"></div>
+
             </div>
             <div class="col-lg-6">
                 <div class="hero-image">
